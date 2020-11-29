4f793360eddedf8adfbc6985f775c267e8bfdbcc
Clint Lawson <lawson.528@osu.edu>
2020-11-29 12:25:09 -0500

    put some sample images in a task4_images folder, wrote 2 different initial versions of task4 using different networks. Both fail to classify images atm

---
+++ task4.py
@@ -0,0 +1,82 @@
+import numpy as np
+from numpy import genfromtxt
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from PIL import Image
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import sys
+import pickle
+import random
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, 3)
+        self.conv2 = nn.Conv2d(64, 128, 3)
+        self.conv3 = nn.Conv2d(128, 256, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = x.view(-1, 1024)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return x
+
+    def processImage(self, imagePath):
+        sourceImg = Image.open(imagePath)
+        resizedImg = sourceImg.resize((32, 32))
+        img = np.reshape(resizedImg, (32, 32, 3), order='F')
+
+        return img
+
+    def plotImage(self, image):
+        plt.figure(figsize=(2, 2))
+        plt.imshow(image)
+        plt.show()
+
+    def normalize(self, image):
+        normImg = np.true_divide(image, 255.0)
+        dataReshaped = np.reshape(normImg, (3, 32, 32))
+
+        return dataReshaped
+
+    def convertToTensor(self, image):
+        tensor = torch.tensor(image, dtype=torch.float32)
+        return tensor
+
+    def predictLabel(self, net, image):
+        image = image.unsqueeze(0)
+        out = net(image)
+        _, prediction = torch.max(out, dim=1)
+        print(prediction)
+    
+def main():
+    net = Net()
+    previous = torch.load('./Saved_Networks/bestNetwork_task3Clint_v2.pth')
+    net.load_state_dict(previous)
+
+    imageFile = sys.argv[1]
+    imagePath = './task4_images/' + imageFile
+    resizedImg = net.processImage(imagePath)
+
+    net.plotImage(resizedImg)
+
+    normalizedImg = net.normalize(resizedImg)
+    tensorImg = net.convertToTensor(normalizedImg)
+
+    net.predictLabel(net, tensorImg)
+
+if __name__ == "__main__":
+    main()
